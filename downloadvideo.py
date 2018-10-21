3887c857972c5c28155bc3304fe26b536e1bd269
ajitW <ajitwagh34@gmail.com>
2018-10-22 02:29:59 +0530

    program done final

--- downloadvideo.py
+++ downloadvideo.py
@@ -1,8 +1,15 @@
 import requests
 from requests.auth import HTTPBasicAuth
 import HTMLParser
+#sudo apt-get install python-lxml
+#pip install --upgrade lxml
 from lxml import etree, html
 parser = HTMLParser.HTMLParser()
+
+SAVE_PATH = "/home/sagar/Ajit/Ajit_Sagar/DownloadUrlVideo"
+
+#link of the video to be downloaded
+links = open('links.txt','r')
 
 
 def get_json_from_form(inp_str):
@@ -23,7 +30,7 @@ def download_file(url):
     local_filename = url.split('/')[-1]
     session = requests.Session()
     
-    payload = {'wa':'wsignin1.0',
+    params = {'wa':'wsignin1.0',
                'wtrealm':'https://ravnurdcw.accesscontrol.windows.net/',
 	       'wreply':'https://ravnurdcw.accesscontrol.windows.net/v2/wsfederation',
 	       'wctx':'cHI9d3NmZWRlcmF0aW9uJnJtPWh0dHBzJTNhJTJmJTJmZGN3YXRlci5yYXZudXIuY29tJTJmJmN4PSUyZkRlZmF1bHQuYXNweA2'
@@ -31,11 +38,12 @@ def download_file(url):
 
     headers = {'Content-Type':'application/x-www-form-urlencoded'
 	      }
-    data = {'UserName':'testlead@dcwater.com',
+    payload = {'UserName':'testlead@dcwater.com',
 	    'Password':'Wipro123$',
 	    'AuthMethod':'FormsAuthentication'}
-    resp = session.post("https://sts.dcwater.com/adfs/ls",params=payload,headers=headers,data = data, verify=False)
-    # rep_cont = parser.unescape(resp.text)
+    resp = session.post("https://sts.dcwater.com/adfs/ls",params=params,headers=headers,data = payload, verify=False)
+    #rep_cont = parser.unescape(resp.text)
+    #import pdb; pdb.set_trace()
     form_data = get_json_from_form(resp.content)
     action = form_data.pop('action')
     resp1 = session.post(action,headers=headers,data=form_data, verify=False)
@@ -49,5 +57,8 @@ def download_file(url):
                 f.write(chunk)
                 #f.flush() commented by recommendation from J.F.Sebastian
     return local_filename
+
+
+   
 
 download_file("https://dcwater.ravnur.com/original/29346/4dbc004e-8341-4573-adc7-28a2097247e1.MPG")
